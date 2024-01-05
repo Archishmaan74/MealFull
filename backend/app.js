29732c2975d5053886494882b879864fa85a46bc
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-05 15:15:27 +0530

    intalled packages & connected database to backend

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mealfullRouter = require('./routes/mealfull');
 
 var app = express();
+var cors = require('cors');
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', mealfullRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
