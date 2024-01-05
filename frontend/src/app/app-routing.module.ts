dfc6c6d4c93651cb666c737c71778b5dff26561a
Archishmaan <archishmaan.upa4@gmail.com>
2024-01-05 16:55:46 +0530

    created routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddfoodComponent } from './addfood/addfood.component';
+import { ChartComponent } from './chart/chart.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'home',  component: HomeComponent
+  },
+  {
+    path: 'add', component: AddfoodComponent
+  },
+  {
+    path: 'chart', component: ChartComponent
+  },
+  {
+    path: '**', component: ErrorpageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
